d1edb2050289d8ee99c58032fecfc8db0e18edcf
calcudaus <dausg878@gmail.com>
2026-04-17 01:34:36 +0700

    feat: implement landing page with SEO meta, structured data, and theme toggle

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -97,8 +97,8 @@
     </script>
 
     {{-- ===== FAVICON ===== --}}
-    <link rel="icon" type="image/webp" href="{{ asset('favicon.webp') }}">
-    <link rel="apple-touch-icon" href="{{ asset('favicon.webp') }}">
+    <link rel="icon" type="image/webp" href="{{ asset('tether-icon-head.webp') }}">
+    <link rel="apple-touch-icon" href="{{ asset('tether-icon-head.webp') }}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
